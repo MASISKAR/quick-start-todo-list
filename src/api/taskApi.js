77ab3d29d6d351579977be84817674da7e4ef403
Masis Karapetyan <masis.karapetyan@protranslating.com>
2023-04-06 21:50:18 +0400

    lesson-70

---
+++ src/api/taskApi.js
@@ -0,0 +1,41 @@
+const taskApiUrl = process.env.REACT_APP_API_URL + "/task";
+
+export default class TaskApi {
+  #request(method, body = null){
+    const params = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if(body !== null){
+      params.body = JSON.stringify(body);
+    }
+    return fetch(taskApiUrl, params).then((result) => result.json());
+  }
+
+  getAll() {
+      return this.#request("GET");
+
+    // return fetch(taskApiUrl, {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // }).then((result) => result.json());
+  }
+  add(task) {
+    return this.#request("POST", task);
+
+    // return fetch(taskApiUrl, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(task),
+    // }).then((result) => result.json());
+  }
+
+  update() {}
+  delete() {}
+}
