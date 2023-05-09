cb20ad090dacb08b2bf8c85d2af22f645a6cee40
Masis Karapetyan <masis.karapetyan@protranslating.com>
2023-05-09 21:56:28 +0400

    sending contact form

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,23 +1,119 @@
-import {useRef, useState} from 'react';
+import { useRef, useState } from "react";
+import { Button } from "react-bootstrap";
+import FormApi from "../../api/formApi";
+import { toast } from "react-toastify";
+import styles from "./contact.module.css";
 
-function Contact(){
-const inputRef = useRef(null);
-const [value, setValue] = useState('');
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+const formApi = new FormApi();
 
-const handleClick = ()=>{
-    console.log(inputRef.current.value);
-    inputRef.current.value = '';
-};
+function Contact() {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+  const [emailErrorMessage, setEmailErrorMessage] = useState(null);
+  const [nameErrorMessage, setNameErrorMessage] = useState(null);
 
-    return(
-        <div>
-        <h1 className="text-center">Contact us page</h1>
-       Input 1 <input type="text" value={value} onChange={(e)=>setValue(e.target.value)}/>
-       <br/>
-        Input 2 <input type="text" ref={inputRef}/>
-        <button onClick={handleClick}>Click me</button>
+  const handleSubmit = async () => {
+    const email = emailRef.current.value;
+    const name = nameRef.current.value;
+    const message = messageRef.current.value;
+    if (!name) {
+      setNameErrorMessage("Name is required!");
+    } else {
+      setNameErrorMessage(null);
+    }
+
+    if (!email) {
+      setEmailErrorMessage("Email address is required!");
+      return;
+    }
+    setEmailErrorMessage(null);
+
+    if (!emailRegex.test(email)) {
+      setEmailErrorMessage("Email address is not valid!");
+      return;
+    }
+    setEmailErrorMessage(null);
+    if (nameErrorMessage) {
+      return;
+    }
+    const form = {
+      name,
+      email,
+      message,
+    };
+    try {
+      await formApi.sendForm(form);
+      toast.success("Thank you for contacting us, the form has been sent!");
+      nameRef.current.value = "";
+      messageRef.current.value = "";
+      emailRef.current.value = "";
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  return (
+    <div className={styles.fill}>
+      <div>
+        <h2 className={styles.contactPageTitle}>
+          We'd Love to hear From You !
+        </h2>
+        <div className={styles.contactForm}>
+          <label htmlFor="name" className={styles.label}>
+            Full name*
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={`${styles.textInput} ${
+              nameErrorMessage ? styles.invalid : ""
+            }`}
+            ref={nameRef}
+          />
+          <label htmlFor="email" className={styles.label}>
+            Email*
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="example@gmail.com"
+            className={`${styles.textInput} ${
+              emailErrorMessage ? styles.invalid : ""
+            }`}
+            ref={emailRef}
+          />
+          <label htmlFor="message" className={styles.label}>
+            Message
+          </label>
+          <textarea
+            id="message"
+            className={styles.textInputs}
+            rows={5}
+            ref={messageRef}
+          />
+          <Button
+            variant="success"
+            className={styles.submit}
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+          {nameErrorMessage && (
+            <h5 className={`${styles.errorMessage} mt-2 p-1`}>
+              {nameErrorMessage}
+            </h5>
+          )}
+          {emailErrorMessage && (
+            <h5 className={`${styles.errorMessage} mt-2 p-1`}>
+              {emailErrorMessage}
+            </h5>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
